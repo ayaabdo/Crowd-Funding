fb9139593873ebb9789d75cb596bdf5fa66897ac
Aya Abdulsamie <33750566+ayaabdo@users.noreply.github.com>
2021-03-27 19:22:25 +0200

    Update settings.py

--- crowdfunding/settings.py
+++ crowdfunding/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fundraising',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,11 +76,13 @@ WSGI_APPLICATION = 'crowdfunding.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'food',
+        'USER': 'django',
+        'PASSWORD': 'django-12345',
+        'HOST': '127.0.0.1',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators

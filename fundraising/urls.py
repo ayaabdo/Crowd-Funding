d52efbb057924b9bf5cccbd1e2932d1d73e02bc9
Aya Abdulsamie <33750566+ayaabdo@users.noreply.github.com>
2021-03-27 23:12:12 +0200

    Update urls.py

--- fundraising/urls.py
+++ fundraising/urls.py
@@ -4,8 +4,6 @@ from .views import projects, home
 
 urlpatterns = [
     path('home', home.index, name='home'),
-    path('item', projects.index, name='item_list'),
-    url(r'^item/(?P<item_id>\d+)/$', item.read, name='item_read'),
-    path('item/add', projects.create, name='item_add'),
+  
 
 ]

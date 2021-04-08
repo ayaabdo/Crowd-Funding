b1d8223cfe819a94a94448f82d473a2d16e49f49
IslamReda <asoeslam@gmail.com>
2021-04-08 13:49:09 +0200

    Add signup module

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,5 @@
-
-from django.urls import path , include
-from django.conf.urls import url 
+from django.urls import path, include
+from django.conf.urls import url
 from . import views
 from .views import UserDelete
 
@@ -8,7 +7,7 @@ urlpatterns = [
     url(r'^profile/(?P<u_id>\d+)/$', views.profile, name='profile'),
     url(r'^edit_profile/(?P<u_id>\d+)/$', views.edit_profile, name='edit_profile'),
     path('<int:pk>/delete', UserDelete.as_view(), name='user_confirm_delete'),
-    url(r'^projects/(?P<u_id>\d+)/$', views.projects, name='projects'), 
-    url(r'^donations/(?P<u_id>\d+)/$', views.donations, name='donations'), 
-   
+    url(r'^projects/(?P<u_id>\d+)/$', views.projects, name='projects'),
+    url(r'^donations/(?P<u_id>\d+)/$', views.donations, name='donations'),
+    url(r'^signup/$', views.signup, name='signup'),
 ]

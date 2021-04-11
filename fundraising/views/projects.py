d35a4c3553cf8bba4fa8eda79a33f9a71f56974d
sondos-medhat <56825620+sondos-medhat@users.noreply.github.com>
2021-04-12 00:03:47 +0200

    Update projects.py

--- fundraising/views/projects.py
+++ fundraising/views/projects.py
@@ -158,5 +158,5 @@ def search(request):
 def delete(request, project_id):
     project = get_object_or_404(Project, id=project_id)
     project.delete()
-    return redirect('view_project', project_id)
-    #return redirect('project_list')
+    #return redirect('view_project', project_id)
+    return redirect('project_list')
